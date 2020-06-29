d4a0d1cbe3db9ba04754c3695cae6b6d0f5b5250
matsurai <m.matsurai25@gmail.com>
2020-06-29 20:51:31 +0900

    update

--- src/const.ts
+++ src/const.ts
@@ -3,3 +3,11 @@ export const colors = {
   black: '#333',
   white: '#fff'
 }
+
+export const media = {
+  large: '@media screen and (min-width: 1600px)',
+  tablet:
+    '@media screen and (min-width: 767px) and (max-width: 1023px)',
+  mobile: '@media screen and (max-width: 767px)',
+  small: '@media screen and (max-width: 350px)'
+}
