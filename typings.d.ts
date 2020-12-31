bbc8a583b419f7abc3749c7fcdb48b7a74cf82b0
matsurai <m.matsurai25@gmail.com>
2020-12-31 10:38:35 +0900

    update

--- typings.d.ts
+++ typings.d.ts
@@ -3,6 +3,12 @@ declare module '*.json' {
   export default value
 }
 
+declare module '*.gif'
+declare module '*.svg'
 declare module '*.png'
 declare module '*.jpg'
 declare module '*.jpeg'
+declare module '*.mp4'
+declare module '*.ico'
+declare module '*.wav'
+declare module '*.zip'
